168c608d90351af1782580f742b846a1010965cb
Tom Hinton <tomjhinton@gmail.com>
2020-01-31 01:19:59 +0000

    deploy test

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var client = new Twitter({
 
 // ars-technica button document.querySelector("#onetrust-accept-btn-handler")
 // guardian document.querySelector("#cmpContainer > div > div > div.css-13we0ur > div.css-14xb8m-buttonContainerStyles > button.css-8k87rr-button-defaultSize-iconDefault-iconRight-mobileButtonStyles")
+// bof document.querySelector("body > div:nth-child(6) > div.bof-main-content > smart-slot > div > div.smart-slot__close-button-container.smart-slot-subscribe-to-daily-digest__close-button-container.smart-slot-campaign-3__close-button-container.smart-slot-position-global-screen-bottom__close-button-container > a > span")
+// document.querySelector("body > div:nth-child(6) > div.bof-main-content > smart-slot > div > div.smart-slot__close-button-container.smart-slot-subscribe-to-daily-digest__close-button-container.smart-slot-campaign-3__close-button-container.smart-slot-position-global-screen-bottom__close-button-container > a > span")
+//document.querySelector("body > div:nth-child(6) > div.bof-main-content > smart-slot > div > div.smart-slot__close-button-container.smart-slot-subscribe-to-daily-digest__close-button-container.smart-slot-campaign-3__close-button-container.smart-slot-position-global-screen-bottom__close-button-container > a > span > i")
 const puppeteer = require('puppeteer')
 setInterval(function () {
 
@@ -55,30 +58,7 @@ let pic
 
 
 
-  //var data = canvas.toBuffer().toString('base64')
 
-  // Make post request on media endpoint. Pass file data as media parameter
-  // client.post('media/upload', {media_data: pic}, function(error, media, response) {
-  //
-  //   if (!error) {
-  //
-  //     // If successful, a media object will be returned.
-  //     console.log(media)
-  //
-  //     // Lets tweet it
-  //     var status = {
-  //       status: '',
-  //       media_ids: media.media_id_string // Pass the media id string
-  //     }
-  //
-  //     client.post('statuses/update', status, function(error, tweet, response) {
-  //       if (!error) {
-  //         console.log(tweet)
-  //       }
-  //     })
-  //
-  //   }
-  // })
 
 }, 40000)
 
@@ -126,29 +106,53 @@ let pic
 
 
 
-  //var data = canvas.toBuffer().toString('base64')
 
-  // Make post request on media endpoint. Pass file data as media parameter
-  // client.post('media/upload', {media_data: pic}, function(error, media, response) {
-  //
-  //   if (!error) {
-  //
-  //     // If successful, a media object will be returned.
-  //     console.log(media)
-  //
-  //     // Lets tweet it
-  //     var status = {
-  //       status: '',
-  //       media_ids: media.media_id_string // Pass the media id string
-  //     }
-  //
-  //     client.post('statuses/update', status, function(error, tweet, response) {
-  //       if (!error) {
-  //         console.log(tweet)
-  //       }
-  //     })
-  //
-  //   }
-  // })
 
 }, 50000)
+
+setInterval(function () {
+
+
+let pic
+  (async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({
+      width: 1280,
+      height: 1920,
+      deviceScaleFactor: 1,
+    })
+    await page.goto('https://www.businessoffashion.com/news');
+    await page.click("body > div:nth-child(6) > div.bof-main-content > smart-slot > div > div.smart-slot__close-button-container.smart-slot-subscribe-to-daily-digest__close-button-container.smart-slot-campaign-3__close-button-container.smart-slot-position-global-screen-bottom__close-button-container > a > span > i");
+    pic = await  page.screenshot({ encoding: "base64" })
+
+    client.post('media/upload', {media_data: pic}, function(error, media, response) {
+
+      if (!error) {
+
+        // If successful, a media object will be returned.
+        console.log(media)
+
+        // Lets tweet it
+        var status = {
+          status: 'https://www.businessoffashion.com/news',
+          media_ids: media.media_id_string // Pass the media id string
+        }
+
+        client.post('statuses/update', status, function(error, tweet, response) {
+          if (!error) {
+            console.log(tweet);
+          }
+        })
+
+      }
+    })
+
+    await browser.close();
+  })();
+
+
+
+
+
+}, 70000)
